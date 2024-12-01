f6db8ae45867062eb868da889ca20fe6be402444
Fil <Pinskiy-FS@yandex.ru>
2024-12-01 11:57:26 +0300

    fix lesson_8

--- src/components/Modal/Comments/Comments.jsx
+++ src/components/Modal/Comments/Comments.jsx
@@ -10,17 +10,28 @@ export const Comments = ({comments}) => {
 
   return (
     <ul className={style.list}>
-      {comments.map(({id, author, body, created_utc: date}) => (
-        <li className={style.item} key={id}>
-          <Text as='h3' className={style.author} size={18} tsize={22}>
-            {author}
-          </Text>
-          <Text as='p' className={style.comment} size={14} tsize={18}>
-            {body}
-          </Text>
-          <DatePost date={date} />
-        </li>
-      ))}
+      {comments.map(({id, author, body, created_utc: date}) => {
+        if (date === undefined) {
+          return (
+            <li className={style.item} key={id}>
+              <Text as='p' className={style.comment} size={14} tsize={18}>
+                Нет даты
+              </Text>
+            </li>
+          );
+        }
+        return (
+          <li className={style.item} key={id}>
+            <Text as='h3' className={style.author} size={18} tsize={22}>
+              {author}
+            </Text>
+            <Text as='p' className={style.comment} size={14} tsize={18}>
+              {body}
+            </Text>
+            <DatePost date={date} />
+          </li>
+        );
+      })}
     </ul>
   );
 };
