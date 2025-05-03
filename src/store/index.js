98d73de2239a642197b10a5cc24ed87da2cd0770
Filipp <PinskiyWorkSpace@yandex.ru>
2025-05-03 12:06:04 +0300

    Lesson_13 no homework

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,13 @@ import { tokenMiddleware, tokenReducer } from './tokenReducer';
 import { commentReducer } from './commentReducer';
 import { authReducer } from './auth/authReducer';
 import postsReducer from './post/postSlice';
+import { searchReducer } from './search/searchReducer';
 import commentsReducer from './commentsData/commentsSlice';
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
@@ -12,8 +17,10 @@ export const store = configureStore({
     auth: authReducer,
     posts: postsReducer,
     comments: commentsReducer,
+    search: searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tokenMiddleware),
+    getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware),
 });
 
+sagaMiddleware.run(rootSaga);
